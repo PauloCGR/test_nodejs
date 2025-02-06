2d5a135f4bd3c646b562ad3efd6e7e6089f6e22b
PauloCGR <pauloruelas2003@gmail.com>
2025-02-05 22:09:10 -0600

    subiendo test de path antes de comenzar primer app

--- 5_path.mjs
+++ 5_path.mjs
@@ -4,3 +4,14 @@ import path from 'node:path'
 console.log(path.sep)
 
 //unir a rutas con path.join
+const filePath = path.join('content', 'subfolder', 'test.txt')
+console.log(filePath)
+
+const base = path.basename('/ruta/rutalarga/files/appdata/etc/file.extension')
+console.log(base)
+
+const filename = path.basename('/ruta/rutalarga/files/appdata/etc/file.extension', '.txt')
+console.log(filename)
+
+var ext = path.extname('image.png')
+console.log(ext)
